fdb16eb2dd79388d1b25fe33c741655579802be4
romie quisido <rome.quisido@gmail.com>
2022-10-07 22:11:34 +0800

    1st commit

--- src/main/kotlin/exercise_1/Main.kt
+++ src/main/kotlin/exercise_1/Main.kt
@@ -1,40 +1,90 @@
+
 fun main() {
-
     /*Application For Employment*/
+    var positionApplyingFor: String? = "Mobile App Developer"
+    var desiredSalary: Int = 50000
+    var dateAvailableForWork: String = "December 15, 2022"
 
-    println("Enter position:")
-    var desiredPosition: String = readln()
-    println("Desired salary:")
-    var desiredSalary: Int = readln().toInt()
-    println("Date available to work:")
-    var dateAvailableEvent: String = readln()
-    println("Last name:")
-    var lastName: String = readln()
-    println("First name:")
-    var firstName: String = readln()
-    println("Middle name:")
-    var middleName: String = readln()
-    println("Enter date of birth:")
-    var dateOfBirth: String = readln().toString()
-    println("Address: Enter city")
-    var address: String = readln()
-    println("State")
-    var state: String = readln()
-    println("Zip")
-    var zip: Int = readln().toInt()
-    println("Homephone")
-    var homePhone: Int = readln().toInt()
-    println("Cell phone")
-    var cellPhone: Long = readln().toLong()
-    println("Email address:")
-    var emailAddress: String = readln()
-    println("Social Security Number")
-    var socialSecurityNumber: Long = readln().toLong()
-    println("Are you a US citizen?:")
-    var nationality: String = readln().toString()
-    println("Have you ever been convicted to a felony?")
-    val convicted: Boolean = readln().toBoolean()
-    println("If selected for employment are you willing to a pre-employment drug testing?")
-    val willing: Boolean = readln().toBoolean()
+    /*Personal Information*/
+
+    var lastName: String = ("Ron")
+    var firstName: String = ("Miller")
+    var middleName: String = ("Ford")
+    var dateOfBirth: String = ("March 17, 1986")
+    var address: String = ("12 Beverly Hills")
+    var state: String = ("Montana")
+    var zip: Int = 9210
+    var homePhone: Long = 333276559
+    var cellPhone: Long = 9367319528
+    var emailAddress: String = ("ronmiller@gmail.com")
+    var socialSecurityNumber: Long = 3322086880
+    var nationality: String = ("null")
+    var hasBeenConvicted: Boolean
+    var willingToDrugTesting: Boolean
+
+    /*Education*/
+
+    var schoolName1: String? = ("null")
+    var schoolLocation1: String? = ("null")
+    var tearsAtttended1: String? = ("null")
+    var degreeReceived1: String? = ("null")
+    var major1: String = ("null")
+
+    var schoolName2: String? = ("null")
+    var schoolLocation2: String? = ("null")
+    var tearsAtttended2: String? = ("null")
+    var degreeReceived2: String? = ("null")
+    var major2: String = ("null")
+
+    var schoolName3: String? = ("null")
+    var schoolLocation3: String? = ("null")
+    var tearsAtttended3: String? = ("null")
+    var degreeReceived3: String? = ("null")
+    var major3: String = ("null")
+
 
 }
+
+
+
+
+//fun main() {
+//
+//    /*Application For Employment*/
+//
+//    println("Enter position:")
+//    var desiredPosition: String = readln()
+//    println("Desired salary:")
+//    var desiredSalary: Int = readln().toInt()
+//    println("Date available to work:")
+//    var dateAvailableEvent: String = readln()
+//    println("Last name:")
+//    var lastName: String = readln()
+//    println("First name:")
+//    var firstName: String = readln()
+//    println("Middle name:")
+//    var middleName: String = readln()
+//    println("Enter date of birth:")
+//    var dateOfBirth: String = readln().toString()
+//    println("Address: Enter city")
+//    var address: String = readln()
+//    println("State")
+//    var state: String = readln()
+//    println("Zip")
+//    var zip: Int = readln().toInt()
+//    println("Homephone")
+//    var homePhone: Int = readln().toInt()
+//    println("Cell phone")
+//    var cellPhone: Long = readln().toLong()
+//    println("Email address:")
+//    var emailAddress: String = readln()
+//    println("Social Security Number")
+//    var socialSecurityNumber: Long = readln().toLong()
+//    println("Are you a US citizen?:")
+//    var nationality: String = readln().toString()
+//    println("Have you ever been convicted to a felony?")
+//    val convicted: Boolean = readln().toBoolean()
+//    println("If selected for employment are you willing to a pre-employment drug testing?")
+//    val willing: Boolean = readln().toBoolean()
+//
+//}

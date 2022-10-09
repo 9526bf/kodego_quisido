026696a15b1f9485873abeaa1cf3759a5dbcbdfa
romie quisido <rome.quisido@gmail.com>
2022-10-09 17:34:47 +0800

    added exercise 1 package added Main.kt in exercise_1 added activity_01_a

--- src/main/kotlin/activities/activity_01_a.kt
+++ src/main/kotlin/activities/activity_01_a.kt
@@ -10,7 +10,7 @@ fun main() {
     val studentControlNumber: Int = readln().toInt()
 
 //Student1
-    var studentControlNumber9526: Int = 9526
+    val studentControlNumber9526: Int = 9526
     val lastName1 = "Johnson"
     val firstName1: String = "David"
     val middleName1: String = "Ford"
@@ -25,17 +25,17 @@ fun main() {
     var score1Week4 = 89
 
     var score1: Int = 90 + 91 + 92 + 89 / 4
-    var grade1: Double = 90.5
+    val grade1: Double = 90.5
 
 //Student 2
-    var studentControlNumber9527: Int = 9527
-    var lastName2: String = "Parkinson"
-    var firstName2: String = "Matthew"
-    var middleName2: String = "Jackson"
-    var dateOfBirth2: String = "July 29, 1984"
-    var address2: String = "Quezon City"
-    var mobileNumber2: Long = 9567766918
-    var classAndSection2: String = "Mobile App Development - MD2P"
+    val studentControlNumber9527: Int = 9527
+    val lastName2: String = "Parkinson"
+    val firstName2: String = "Matthew"
+    val middleName2: String = "Jackson"
+    val dateOfBirth2: String = "July 29, 1984"
+    val address2: String = "Quezon City"
+    val mobileNumber2: Long = 9567766918
+    val classAndSection2: String = "Mobile App Development - MD2P"
 
     var score2Week1 = 89
     var score2Week2 = 88
@@ -43,57 +43,67 @@ fun main() {
     var score2Week4 = 89
 
     var score2: Int = 90 + 91 + 92 + 89 / 4
-    var grade2: Double = 89.25
+    val grade2: Double = 89.25
 
 //Student 3
-    var studentControlNumber9528: Int = 9528
-    var lastName3: String = "Collins"
-    var firstName3: String = "Simon"
-    var middleName3: String = "Monroe"
-    var dateOfBirth3: String = "July 2, 1986"
-    var address3: String = "Cebu City"
-    var mobileNumber3: Long = 9213685178
-    var classAndSection3: String = "Mobile App Development - MD2P"
+    val studentControlNumber9528: Int = 9528
+    val lastName3: String = "Collins"
+    val firstName3: String = "Simon"
+    val middleName3: String = "Monroe"
+    val dateOfBirth3: String = "July 2, 1986"
+    val address3: String = "Cebu City"
+    val mobileNumber3: Long = 9213685178
+    val classAndSection3: String = "Mobile App Development - MD2P"
 
-    var score3Week1 = 89
-    var score3Week2 = 88
-    var score3Week3 = 91
-    var score3Week4 = 90
+    var score3Week1 = 87
+    var score3Week2 = 85
+    var score3Week3 = 80
+    var score3Week4 = 75
+
+    var score3: Int = 87 + 85 + 80 + 75 / 4
+    val grade3: Double = 81.75
 
 //Student 4
-    var studentControlNumber9529: Int = 9529
-    var lastName4: String = "Jefferson"
-    var firstName4: String = "Jay"
-    var middleName4: String = "Jones"
-    var dateOfBirth4: String = "April 2, 1985"
-    var address4: String = "Davao City"
-    var mobileNumber4: Long = 9367329568
-    var classAndSection4: String = "Mobile App Development - MD2P"
+    val studentControlNumber9529: Int = 9529
+    val lastName4: String = "Jefferson"
+    val firstName4: String = "Jay"
+    val middleName4: String = "Jones"
+    val dateOfBirth4: String = "April 2, 1985"
+    val address4: String = "Davao City"
+    val mobileNumber4: Long = 9367329568
+    val classAndSection4: String = "Mobile App Development - MD2P"
 
     var score4Week1 = 91
     var score4Week2 = 89
     var score4Week3 = 87
     var score4Week4 = 92
 
+    var score4: Int = 91 + 89 + 87 + 92 / 4
+    val grade4: Double = 89.75
+
+
 //Student 5
-    var studentControlNumber9530: Int = 9530
-    var lastName5: String = "Monroe"
-    var firstName5: String = "Marilyn"
-    var middleName5: String = "Joe"
-    var dateOfBirth5: String = "April 27, 1980"
-    var address5: String = "Valenzuela City"
-    var mobileNumber5: Long = 9394186347
-    var classAndSection5: String = "Mobile App Development - MD2P"
+    val studentControlNumber9530: Int = 9530
+    val lastName5: String = "Monroe"
+    val firstName5: String = "Marilyn"
+    val middleName5: String = "Joe"
+    val dateOfBirth5: String = "April 27, 1980"
+    val address5: String = "Valenzuela City"
+    val mobileNumber5: Long = 9394186347
+    val classAndSection5: String = "Mobile App Development - MD2P"
 
     var score5Week1 = 73
     var score5Week2 = 74
     var score5Week3 = 75
     var score5Week4 = 76
 
-    var studentControlNumberRange = 9526..9530
+    var score5: Int = 73 + 74 + 75 + 76 / 4
+    val grade5: Double = 74.5
+
+   // var studentControlNumberRange = 9526..9530
 
     if (studentControlNumber == 9526) {
-        println("Control Number: $studentControlNumber")
+        println("Control Number: $studentControlNumber9526")
         println("Full name: $lastName1, $firstName1 $middleName1")
         println("Date of birth: $dateOfBirth1")
         println("Address: $address1")
@@ -101,18 +111,47 @@ fun main() {
         println("Class and section: $classAndSection1")
         println("grade: $grade1")
 
-        if (studentControlNumber == 9527) {
-            println("Control Number: $studentControlNumber")
-            println("Full name: $lastName2, $firstName2 $middleName2")
-            println("Date of birth: $dateOfBirth2")
-            println("Address: $address2")
-            println("Mobile number: $mobileNumber2")
-            println("Class and section: $classAndSection2")
-            println("grade: $grade2")
+    }
+    if (studentControlNumber == 9527) {
+        println("Control Number: $studentControlNumber9527")
+        println("Full name: $lastName2, $firstName2 $middleName2")
+        println("Date of birth: $dateOfBirth2")
+        println("Address: $address2")
+        println("Mobile number: $mobileNumber2")
+        println("Class and section: $classAndSection2")
+        println("grade: $grade2")
 
-        }
+    }
+    if (studentControlNumber == 9528) {
+        println("Control Number: $studentControlNumber9528")
+        println("Full name: $lastName3, $firstName3 $middleName3")
+        println("Date of birth: $dateOfBirth3")
+        println("Address: $address3")
+        println("Mobile number: $mobileNumber3")
+        println("Class and section: $classAndSection3")
+        println("grade: $grade3")
+
+    }
+    if (studentControlNumber == 9529) {
+        println("Control Number: $studentControlNumber9529")
+        println("Full name: $lastName4, $firstName4 $middleName4")
+        println("Date of birth: $dateOfBirth4")
+        println("Address: $address4")
+        println("Mobile number: $mobileNumber4")
+        println("Class and section: $classAndSection4")
+        println("grade: $grade4")
+    }
+    if (studentControlNumber == 9530) {
+        println("Control Number: $studentControlNumber9530")
+        println("Full name: $lastName5, $firstName5 $middleName5")
+        println("Date of birth: $dateOfBirth5")
+        println("Address: $address5")
+        println("Mobile number: $mobileNumber5")
+        println("Class and section: $classAndSection5")
+        println("grade: $grade5")
     }
 }
+
 
 
 
